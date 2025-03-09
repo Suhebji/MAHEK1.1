5047f523e86920b9f4d93392cb0fb8771d576cc4
rdxlabv6 <rdxsardar9@gmail.com>
2025-03-09 13:17:50 +0500

    Update ai-siri.js

--- Priyansh/commands/ai-siri.js
+++ Priyansh/commands/ai-siri.js
@@ -13,16 +13,16 @@ const config = {
 
 const handleEvent = async function ({ api, event, client, __GLOBAL }) {
 
-  if (event.body.indexOf("siri") === 0 || event.body.indexOf("Siri") === 0 || event.body.indexOf("zain") === 0 || event.body.indexOf("Zain") === 0)  {
+  if (event.body.indexOf("siri") === 0 || event.body.indexOf("Siri") === 0 || event.body.indexOf("mahek") === 0 || event.body.indexOf("jutti") === 0)  {
     const { threadID, messageID } = event;
     const input = event.body;
     const message = input.split(" ");
 
     if (message.length < 2) {
-      api.sendMessage("✨ 𝙷𝚎𝚕𝚕𝚘 𝙸 𝙰𝚖 zain bot kia ap bta sakty hy ap ko mare owner sa kia kam hy agr ap kuch time wait kr sakty hy to fine agr zrori baat krni hy to ma unha inbox ma msg kr ka bula sakta hu thanks ♥️♥️ ", event.threadID);
+      api.sendMessage("✨ 𝙷𝚎𝚕𝚕𝚘 𝙸 𝙰𝚖 mahek bot kia ap bta sakty hy ap ko mare owner sa kia kam hy agr ap kuch time wait kr sakty hy to fine agr zrori baat krni hy to ma unha inbox ma msg kr ka bula sakta hu thanks ♥️♥️ ", event.threadID);
     } else {
       try {
-        api.sendMessage(`ℤ𝔸𝕀ℕ 𝔹𝕆𝕋 𝙸𝚜 𝚆𝚘𝚛𝚔𝚒𝚗𝚐`, event.threadID);
+        api.sendMessage(`𝕞𝕒𝕙𝕒𝕜 𝕛𝕦𝕥𝕥𝕚 𝙸𝚜 𝚆𝚘𝚛𝚔𝚒𝚗𝚐`, event.threadID);
         const ris = await axios.get(`https://vw6v4g-3000.csb.app/api/tools/bard?question=${message.slice(1).join(" ")}`);
         const resultai = ris.data.edtmsg;
 
@@ -30,7 +30,7 @@ const handleEvent = async function ({ api, event, client, __GLOBAL }) {
     api.sendMessage(`${resultai}\n\n\n༺═─────────═༻\n𝚃𝚑𝚒𝚜 𝙸𝚜 𝙰𝚗 𝙰𝚒 𝙻𝚒𝚔𝚎 𝙱𝚊𝚛𝚍 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢 𝙽𝚊𝚞𝚐𝚑𝚝𝚢 𝙰𝚗𝚍 𝙸𝚝 𝙰𝚕𝚜𝚘 𝙷𝚊𝚟𝚎 𝚁𝚎𝚊𝚕-𝚝𝚒𝚖𝚎 𝙳𝚊𝚝𝚊 𝙰𝚌𝚎𝚜𝚜 \n༺═─────────═༻`, event.threadID);
   } catch (err) {
         console.error(err);
-        api.sendMessage("𝙷𝚎𝚕𝚕𝚘 𝚉𝚊𝚒𝚗 𝚊𝚋𝚑𝚒 𝚋𝚞𝚜𝚢 𝚑𝚢 𝚊𝚙 𝚝𝚑𝚘𝚛𝚊 𝚠𝚊𝚒𝚝 𝚔𝚛 𝚕𝚢 𝚊𝚐𝚛 𝚘𝚠𝚗𝚎𝚛 𝚗𝚑𝚒 𝚊𝚝𝚢 𝚝𝚘 𝚖𝚊 𝚞𝚗𝚑𝚊 𝚒𝚋 𝚖𝚊 𝚖𝚜𝚐 𝚔𝚛 𝚜𝚊𝚔𝚝𝚊 𝚊𝚐𝚛 𝚣𝚊𝚢𝚊𝚍𝚊 𝚣𝚛𝚘𝚛𝚒 𝚋𝚊𝚊𝚝 𝚑𝚢 𝚝𝚘 𝚊𝚙 𝚒𝚋 𝚖𝚎 𝚖𝚜𝚐 𝚔𝚛 𝚜𝚊𝚔𝚝𝚢 𝚑𝚢 ♥️♥️ owner id♥️https://www.facebook.com/profile.php?id=100086033644262&mibextid=ZbWKwL", event.threadID);
+        api.sendMessage("𝙷𝚎𝚕𝚕𝚘 jutti  𝚊𝚋𝚑𝚒 𝚋𝚞𝚜𝚢 𝚑𝚢 𝚊𝚙 𝚝𝚑𝚘𝚛𝚊 𝚠𝚊𝚒𝚝 𝚔𝚛 𝚕𝚢 𝚊𝚐𝚛 𝚘𝚠𝚗𝚎𝚛 𝚗𝚑𝚒 𝚊𝚝𝚢 𝚝𝚘 𝚖𝚊 𝚞𝚗𝚑𝚊 𝚒𝚋 𝚖𝚊 𝚖𝚜𝚐 𝚔𝚛 𝚜𝚊𝚔𝚝𝚊 𝚊𝚐𝚛 𝚣𝚊𝚢𝚊𝚍𝚊 𝚣𝚛𝚘𝚛𝚒 𝚋𝚊𝚊𝚝 𝚑𝚢 𝚝𝚘 𝚊𝚙 𝚒𝚋 𝚖𝚎 𝚖𝚜𝚐 𝚔𝚛 𝚜𝚊𝚔𝚝𝚢 𝚑𝚢 ♥️♥️ owner id♥️https://www.facebook.com/share/1ZP2MFiPSR/", event.threadID);
   }
       }
         }
